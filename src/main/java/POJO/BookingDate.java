e5edc2dfc646bf4bb12ea531e7e0c6ea26c7c1da
HagerEslam5 <159949293+HagerEslam5@users.noreply.github.com>
2024-07-11 12:26:55 +0300

    Update BookingDate.java

--- src/main/java/POJO/BookingDate.java
+++ src/main/java/POJO/BookingDate.java
@@ -3,22 +3,22 @@ package POJO;
 public class BookingDate {
 
 	private String checkin;
+	private String checkout;
+
 	public String getCheckin() {
 		return checkin;
 	}
-	
-	public BookingDate() {
-		this.checkin="2018-01-01";
-		this.checkout="2019-01-01";
-	}
+
 	public void setCheckin(String checkin) {
 		this.checkin = checkin;
 	}
+
 	public String getCheckout() {
 		return checkout;
 	}
+
 	public void setCheckout(String checkout) {
 		this.checkout = checkout;
 	}
-	private String checkout;
+
 }
