29b0389d3d43e64959c80895f14076c68000c0ea
HagerEslam5 <159949293+HagerEslam5@users.noreply.github.com>
2024-07-17 12:05:56 +0300

    Update MainSteps.java

--- src/test/java/stepdefinitions/MainSteps.java
+++ src/test/java/stepdefinitions/MainSteps.java
@@ -6,9 +6,13 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.testng.Assert;
+
+import com.github.javafaker.Faker;
 
 import POJO.Booking;
 import POJO.BookingDate;
@@ -19,16 +23,16 @@ import io.cucumber.java.en.When;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import utilities.*;
 
 public class MainSteps extends AbstractComponents {
 	TestData TestData = new TestData();
+	Faker faker = new Faker();
 	private RequestSpecification request;
 	private Response resBody;
 	private static String token;
 	private static int bookingID;
-	private String fName = "Micheal";
-	private String lName = "Scott";
 
 	@Given("user credentials")
 	public void user_credentials(List<String> data) throws IOException {
@@ -38,22 +42,23 @@ public class MainSteps extends AbstractComponents {
 
 	@Given("{string}API payload")
 	public void payload(String api) throws IOException {
+		SimpleDateFormat SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		RequestSpecification reqSpec = buildRequestSpec();
 		if (api.equalsIgnoreCase("CreateBooking")) {
 			Booking Booking = new Booking();
-			Booking.setFirstname("Jim");
-			Booking.setLastname("Brown");
-			Booking.setTotalprice("111");
+			Booking.setFirstname(faker.name().firstName());
+			Booking.setLastname(faker.name().lastName());
+			Booking.setTotalprice(faker.number().numberBetween(50, 100));
 			Booking.setDepositpaid(true);
-			Booking.setAdditionalneeds("Breakfast");
+			Booking.setAdditionalneeds(faker.job().field());
 			BookingDate BookingDate = new BookingDate();
-			BookingDate.setCheckin("2018-01-01");
-			BookingDate.setCheckout("2019-01-01");
+			BookingDate.setCheckin(SimpleDateFormat.format(faker.date().past(20, TimeUnit.DAYS)));
+			BookingDate.setCheckout(SimpleDateFormat.format(faker.date().future(10, TimeUnit.DAYS)));
 			Booking.setBookingdates(BookingDate);
 			request = given().spec(reqSpec).body(Booking);
 		} else if (api.equalsIgnoreCase("PartialUpdateBooking")) {
 			request = given().spec(reqSpec).pathParam("bookingID", bookingID).header("Cookie", "token=" + token)
-					.body(TestData.UpdateDataPartially(fName, lName));
+					.body(TestData.UpdateDataPartially(faker.name().firstName(), faker.name().lastName()));
 		}
 	}
 
@@ -76,8 +81,10 @@ public class MainSteps extends AbstractComponents {
 			resBody = request.when().delete(resourceAPI.getResource());
 	}
 
-	@Then("The API call is success with status code {int} and response time less than 3000L")
+	@Then("The API call is successful with status code {int} and response time less than 20000L")
 	public void the_api_call_is_success_with_status_code_and_response_time_less_than_3000l(Integer expectedStatusCode) {
+		ResponseSpecification resSpec = buildResponseSpec();
+		resBody.then().spec(resSpec);
 		Assert.assertEquals(resBody.getStatusCode(), expectedStatusCode);
 	}
 
@@ -100,7 +107,7 @@ public class MainSteps extends AbstractComponents {
 
 		Assert.assertEquals(js.getString("firstname"), POJORes.getFirstname());
 		Assert.assertEquals(js.getString("lastname"), POJORes.getLastname());
-		Assert.assertEquals(js.getString("totalprice"), POJORes.getTotalprice());
+		Assert.assertEquals(js.getInt("totalprice"), POJORes.getTotalprice());
 		Assert.assertEquals(js.getBoolean("depositpaid"), POJORes.getDepositpaid());
 		Assert.assertEquals(js.getString("additionalneeds"), POJORes.getAdditionalneeds());
 		Assert.assertEquals(js.getString("bookingdates.checkin"), POJORes.getBookingdates().getCheckin());
@@ -110,7 +117,8 @@ public class MainSteps extends AbstractComponents {
 
 	@Then("data is updated successfully")
 	public void data_is_updated_successfully() {
-		resBody.then().assertThat().body("firstname", equalTo(fName)).assertThat().body("lastname", equalTo(lName));
+		POJO.Booking POJORes = resBody.then().extract().as(Booking.class);
+		resBody.then().assertThat().body("firstname", equalTo(POJORes.getFirstname())).assertThat().body("lastname", equalTo(POJORes.getLastname()));
 	}
 
 	@Then("Data is deleted successfully")
